363437568a63fc747f34c99712c118b0dbee350a
Avi Avinav <aviavinav2004@gmail.com>
2024-08-08 22:24:21 +0530

    simpole

---
+++ main.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn");
+const lbl = document.getElementsByTagName("label")[0];
+
+btn.onclick = () => (lbl.textContent = "Fooled you!");
+
+const FirstHalfOfTheFlag = atob("b3dhc3B7amF2YXNjcmlwdF8");
+const SecondHalfOfTheFlag = "synt!}"; // ROT away!
