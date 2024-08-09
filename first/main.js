831265270a10c663edd05d17cda24cfe37ca26c4
Avi Avinav <aviavinav2004@gmail.com>
2024-08-09 22:49:49 +0530

    replace flag format from owasp to d4rkc0de

--- first/main.js
+++ first/main.js
@@ -3,5 +3,5 @@ const lbl = document.getElementsByTagName("label")[0];
 
 btn.onclick = () => (lbl.innerText = "Fooled you!");
 
-const FirstHalfOfTheFlag = atob("b3dhc3B7amF2YXNjcmlwdF8");
+const FirstHalfOfTheFlag = atob("ZDRya2MwZGV7amF2YXNjcmlwdF8=");
 const SecondHalfOfTheFlag = "synt!}"; // ROT away!
